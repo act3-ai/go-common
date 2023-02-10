12b86bc1ba4d0be514b176ca864db29bd28c208d
Justen Stall <justenstall@gmail.com>
2023-02-10 10:56:41 -0500

    fix: support passing additional manpage documents to gendocs command

--- pkg/cmd/gendocs.go
+++ pkg/cmd/gendocs.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
+
+// GendocsOptions stores options for the gendocs command
+type GendocsOptions struct {
+	Format             string            // Documentation format, either "md" or "man"
+	AdditionalManpages map[string][]byte // Non-generatable man pages to be included (ex. Quick Start Guides, User Guides)
+}
 
 // NewGendocsCmd is a command to generate the internal CLI documentation in markdown
 func NewGendocsCmd() *cobra.Command {
@@ -29,4 +37,24 @@ func NewGendocsCmd() *cobra.Command {
 	}
 	gendocsCmd.Flags().StringVarP(&format, "format", "f", "md", "Set output documentation format. Supports \"md\" for markdown or \"man\" for manpage")
 	return gendocsCmd
+}
+
+// Run runs the Gendocs action
+func (action *GendocsOptions) Run(ctx context.Context, cmd *cobra.Command, docsPath string) error {
+	if action.Format == "md" {
+		return doc.GenMarkdownTree(cmd.Root(), docsPath) //nolint:wrapcheck
+	} else if action.Format == "man" {
+		err := doc.GenManTree(cmd.Root(), nil, docsPath)
+		if err != nil {
+			return err //nolint:wrapcheck
+		}
+
+		for name, content := range action.AdditionalManpages {
+			if err := os.WriteFile(name, content, 0666); err != nil {
+				return err //nolint:wrapcheck
+			}
+		}
+	}
+
+	return fmt.Errorf("incorrect value for format")
 }
