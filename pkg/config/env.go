21127851869143641f33b02dc0f75e0045f5daa0
Jack Dubbs <jack.dubbs@jacobs.com>
2023-06-08 12:08:19 +0000

    Add more (config) env funcs

--- pkg/config/env.go
+++ pkg/config/env.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -8,10 +9,18 @@ import (
 	"time"
 )
 
+// define errors for config
+var (
+	// ErrEnvVarNotFound is returned when an environment variable is not found (os.LookupEnv error)
+	ErrEnvVarNotFound = errors.New("environment variable not found")
+	// ErrParseEnvVar is returned when an environment variable is found but cannot be parsed
+	ErrParseEnvVar = errors.New("error parsing environment variable")
+)
+
 // EnvOr grabs the env variable or the default
 func EnvOr(name, def string) string {
 	if name == "" {
-		return def
+		panic("name must not be empty")
 	}
 	envVal, ok := os.LookupEnv(name)
 	if !ok {
@@ -20,10 +29,23 @@ func EnvOr(name, def string) string {
 	return envVal
 }
 
+// Env returns the named env variable if it exists,
+// otherwise returns empty string and an ErrEnvVarNotFound error.
+func Env(name string) (string, error) {
+	if name == "" {
+		panic("name must not be empty")
+	}
+	envVal, ok := os.LookupEnv(name)
+	if !ok {
+		return "", ErrEnvVarNotFound
+	}
+	return envVal, nil
+}
+
 // EnvIntOr grabs the env variable as an int or the default
 func EnvIntOr(name string, def int) int {
 	if name == "" {
-		return def
+		panic("name must not be empty")
 	}
 	envVal, ok := os.LookupEnv(name)
 	if !ok {
@@ -36,10 +58,27 @@ func EnvIntOr(name string, def int) int {
 	return ret
 }
 
+// EnvInt returns the named env variable if it exists,
+// otherwise returns 0 and either an ErrEnvVarNotFound or an ErrParseEnvVar error.
+func EnvInt(name string) (int, error) {
+	if name == "" {
+		panic("name must not be empty")
+	}
+	envVal, ok := os.LookupEnv(name)
+	if !ok {
+		return 0, ErrEnvVarNotFound
+	}
+	parsedVal, err := strconv.Atoi(envVal)
+	if err != nil {
+		return 0, ErrParseEnvVar
+	}
+	return parsedVal, nil
+}
+
 // EnvBoolOr grabs the env variable as an int or the default
 func EnvBoolOr(name string, def bool) bool {
 	if name == "" {
-		return def
+		panic("name must not be empty")
 	}
 	envVal, ok := os.LookupEnv(name)
 	if !ok {
@@ -52,10 +91,27 @@ func EnvBoolOr(name string, def bool) bool {
 	return ret
 }
 
+// EnvBool returns the named env variable if it exists,
+// otherwise returns false and either an ErrEnvVarNotFound or an ErrParseEnvVar error.
+func EnvBool(name string) (bool, error) {
+	if name == "" {
+		panic("name must not be empty")
+	}
+	envVal, ok := os.LookupEnv(name)
+	if !ok {
+		return false, ErrEnvVarNotFound
+	}
+	parsedVal, err := strconv.ParseBool(envVal)
+	if err != nil {
+		return false, ErrParseEnvVar
+	}
+	return parsedVal, nil
+}
+
 // EnvArrayOr grabs the env variable as an array.  Returns an empty array if
 func EnvArrayOr(name string, def []string, sep string) []string {
 	if name == "" {
-		return def
+		panic("name must not be empty")
 	}
 	envVal, ok := os.LookupEnv(name)
 	if !ok || envVal == "" {
@@ -72,7 +128,7 @@ func EnvPathOr(name string, def []string) []string {
 // EnvDurationOr grabs the env variable as a Duration or the default
 func EnvDurationOr(name string, def time.Duration) time.Duration {
 	if name == "" {
-		return def
+		panic("name must not be empty")
 	}
 	envVal, ok := os.LookupEnv(name)
 	if !ok {
@@ -83,4 +139,21 @@ func EnvDurationOr(name string, def time.Duration) time.Duration {
 		return def
 	}
 	return ret
+}
+
+// EnvDuration returns the named env variable if it exists,
+// otherwise returns 0 and either an ErrEnvVarNotFound or an ErrParseEnvVar error.
+func EnvDuration(name string) (time.Duration, error) {
+	if name == "" {
+		panic("name must not be empty")
+	}
+	envVal, ok := os.LookupEnv(name)
+	if !ok {
+		return 0, ErrEnvVarNotFound
+	}
+	parsedVal, err := time.ParseDuration(envVal)
+	if err != nil {
+		return 0, ErrParseEnvVar
+	}
+	return parsedVal, nil
 }
