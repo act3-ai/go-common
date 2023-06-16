63cb1cf07139f350556ea9e2e1fa33f96e6da427
Kyle M. Tarplee <kyle.tarplee@udri.udayton.edu>
2023-06-16 08:12:51 -0400

    Moved "deep" into an option and made it the default

--- pkg/fsutil/equality.go
+++ pkg/fsutil/equality.go
@@ -2,6 +2,7 @@ package fsutil
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"io/fs"
@@ -9,21 +10,30 @@ import (
 	"strings"
 )
 
-// DeepEqualFilesystem checks that the filesystems (excluding hidden files/dirs) are identical.
-// Equality based on comparion opts.
-// It also checks that matching files have identical content.
-func DeepEqualFilesystem(fsA, fsB fs.FS, opts ComparisonOpts) error {
-	return equalFilesystem(fsA, fsB, opts, true)
+// ComparisonOpts stores options for comparing file system equality
+type ComparisonOpts struct {
+	Name    bool // Compare name
+	Size    bool // Compare size
+	Mode    bool // Compare mode
+	Content bool // Compare file content
+}
+
+// DefaultComparisonOpts compares only the name, size, dir, and mode of fs.FileInfo
+var DefaultComparisonOpts = ComparisonOpts{
+	Name:    true,
+	Size:    true,
+	Mode:    true,
+	Content: true,
 }
 
 // EqualFilesystem checks that the filesystems (excluding hidden files/dirs) are identical.
-// Equality based on comparion opts.
+// Equality based on comparison options.
 func EqualFilesystem(fsA, fsB fs.FS, opts ComparisonOpts) error {
-	return equalFilesystem(fsA, fsB, opts, false)
+	return equalFilesystem(fsA, fsB, opts)
 }
 
 // equalFilesystem checks that the filesystems (excluding hidden files/dirs) are identical.
-func equalFilesystem(fsA, fsB fs.FS, opts ComparisonOpts, deep bool) (err error) {
+func equalFilesystem(fsA, fsB fs.FS, opts ComparisonOpts) (err error) {
 	fsInfoA, err := getFSInfo(fsA)
 	if err != nil {
 		return fmt.Errorf("failed to get fsInfo for fsA: %w", err)
@@ -41,12 +51,11 @@ func equalFilesystem(fsA, fsB fs.FS, opts ComparisonOpts, deep bool) (err error)
 		if err := compareFinfo(path, infoA, infoB, opts); err != nil {
 			return err
 		}
-		if deep {
+		if opts.Content {
 			if err := openAndCompare(fsA, fsB, path); err != nil {
 				return fmt.Errorf("failed to compare file contents for path %s: %w", path, err)
 			}
 		}
-
 	}
 
 	for path, infoA := range fsInfoA.dirs {
@@ -62,21 +71,15 @@ func equalFilesystem(fsA, fsB fs.FS, opts ComparisonOpts, deep bool) (err error)
 	return nil
 }
 
-// DeepDiffFS returns the differences between two filesystems. (A-B)
-// File contents are also compared
-func DeepDiffFS(fsA, fsB fs.FS, opts ComparisonOpts) ([]fs.FileInfo, error) {
-	return diffFS(fsA, fsB, opts, true)
-}
-
 // DiffFS returns the differences between two filesystems. (A-B)
 func DiffFS(fsA, fsB fs.FS, opts ComparisonOpts) ([]fs.FileInfo, error) {
-	return diffFS(fsA, fsB, opts, false)
+	return diffFS(fsA, fsB, opts)
 }
 
 // diffFS returns the differences between two filesystems. (A-B)
 // differences are determined by opts.
 // if deep is true, then the contents of files are also compared.
-func diffFS(fsA, fsB fs.FS, opts ComparisonOpts, deep bool) ([]fs.FileInfo, error) {
+func diffFS(fsA, fsB fs.FS, opts ComparisonOpts) ([]fs.FileInfo, error) {
 	fsInfoA, err := getFSInfo(fsA)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get fsInfo for fsA: %w", err)
@@ -100,7 +103,7 @@ func diffFS(fsA, fsB fs.FS, opts ComparisonOpts, deep bool) ([]fs.FileInfo, erro
 			diffs = append(diffs, infoA)
 			// if no differences in file info, and deep, compare file contents
 			// no need to compare contents if there are differences in file info
-		} else if deep {
+		} else if opts.Content {
 			if err := openAndCompare(fsA, fsB, path); err != nil {
 				diffs = append(diffs, infoA)
 			}
@@ -162,22 +165,6 @@ func getFSInfo(fsys fs.FS) (*fsInfo, error) {
 	return fsI, nil
 }
 
-// ComparisonOpts stores options for comparing fs.FileInfo equality
-type ComparisonOpts struct {
-	Name bool // Compare name
-	Size bool // Compare size
-	Mode bool // Compare mode
-}
-
-var (
-	// DefaultComparisonOpts compares only the name, size, dir, and mode of fs.FileInfo
-	DefaultComparisonOpts = ComparisonOpts{
-		Name: true,
-		Size: true,
-		Mode: true,
-	}
-)
-
 func compareFinfo(path string, a, b fs.FileInfo, opts ComparisonOpts) error {
 	if opts.Name && a.Name() != b.Name() {
 		return fmt.Errorf("names should be equal for path: %s, a: %s, b: %s", path, a.Name(), b.Name())
@@ -202,26 +189,31 @@ func openAndCompare(a fs.FS, b fs.FS, path string) (err error) {
 	}
 	defer func() {
 		closeErr := fA.Close()
-		if err != nil {
+		if err == nil {
 			err = closeErr
 		}
 	}()
+
 	fB, err := b.Open(path)
 	if err != nil {
 		return fmt.Errorf("failed to open file in fsB: %w", err)
 	}
 	defer func() {
 		closeErr := fB.Close()
-		if err != nil {
+		if err == nil {
 			err = closeErr
 		}
 	}()
-	return compareFileContents(fA, fB)
+
+	if err := compareFileContents(fA, fB); err != nil {
+		return fmt.Errorf("failed to compare file : %w", err)
+	}
+	return nil
 }
 
 // compareFileContents compares the contents of two files.
-func compareFileContents(a, b fs.File) error {
-	bufSize := 4096
+func compareFileContents(a, b io.Reader) error {
+	const bufSize = 32 * 1024
 	bufA := make([]byte, bufSize)
 	bufB := make([]byte, bufSize)
 
@@ -233,15 +225,15 @@ func compareFileContents(a, b fs.File) error {
 			return fmt.Errorf("files are not equal")
 		}
 
-		if errA == io.EOF && errB == io.EOF {
+		if errors.Is(errA, io.EOF) && errors.Is(errB, io.EOF) {
 			break
 		}
 
-		if errA != nil && errA != io.ErrUnexpectedEOF {
+		if errA != nil && !errors.Is(errA, io.ErrUnexpectedEOF) {
 			return fmt.Errorf("failed to read from fileA: %w", errA)
 		}
 
-		if errB != nil && errB != io.ErrUnexpectedEOF {
+		if errB != nil && !errors.Is(errB, io.ErrUnexpectedEOF) {
 			return fmt.Errorf("failed to read from fileB: %w", errB)
 		}
 	}
--- pkg/fsutil/equality_test.go
+++ pkg/fsutil/equality_test.go
@@ -8,210 +8,185 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testCases() []struct {
-	name        string
-	fsA         fstest.MapFS
-	fsB         fstest.MapFS
-	opts        ComparisonOpts
-	shouldError bool
-	expectedLen int
-} {
-	return []struct {
-		name        string
-		fsA         fstest.MapFS
-		fsB         fstest.MapFS
-		opts        ComparisonOpts
-		shouldError bool
-		expectedLen int
-	}{
-		{
-			name:        "Empty filesystems",
-			fsA:         fstest.MapFS{},
-			fsB:         fstest.MapFS{},
-			opts:        DefaultComparisonOpts,
-			shouldError: false,
-			expectedLen: 0,
-		},
-		{
-			name: "Identical filesystems",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: false,
-			expectedLen: 0,
-		},
-		{
-			name: "Different filesystems",
-			fsA: fstest.MapFS{
-				"fileA.txt": &fstest.MapFile{Data: []byte("File A content")},
-			},
-			fsB: fstest.MapFS{
-				"fileB.txt": &fstest.MapFile{Data: []byte("File B content")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Filesystem with Info() error",
-			fsA: fstest.MapFS{
-				"file.txt":       &fstest.MapFile{Data: []byte("File content")},
-				"error_info.txt": &fstest.MapFile{Data: []byte("Error content")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt":       &fstest.MapFile{Data: []byte("File content")},
-				"error_info.txt": &fstest.MapFile{Data: []byte("Error content")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 0,
-		},
-		{
-			name: "Mismatched names",
-			fsA: fstest.MapFS{
-				"file_a.txt": &fstest.MapFile{Data: []byte("hello")},
-			},
-			fsB: fstest.MapFS{
-				"file_b.txt": &fstest.MapFile{Data: []byte("hello")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched sizes",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("hello")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("hello world")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched modes",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: 0600},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: 0644},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched directory status",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("content")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: fs.ModeDir},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Directory missing in fsB",
-			fsA: fstest.MapFS{
-				"dir_a": &fstest.MapFile{Mode: fs.ModeDir},
-			},
-			fsB:         fstest.MapFS{}, // Empty MapFS
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched directory names",
-			fsA: fstest.MapFS{
-				"dir_a": &fstest.MapFile{Mode: fs.ModeDir},
-			},
-			fsB: fstest.MapFS{
-				"dir_b": &fstest.MapFile{Mode: fs.ModeDir},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched directory modes",
-			fsA: fstest.MapFS{
-				"dir": &fstest.MapFile{Mode: fs.ModeDir | 0755},
-			},
-			fsB: fstest.MapFS{
-				"dir": &fstest.MapFile{Mode: fs.ModeDir | 0700},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-		{
-			name: "Mismatched directory status",
-			fsA: fstest.MapFS{
-				"dir": &fstest.MapFile{Mode: fs.ModeDir},
-			},
-			fsB: fstest.MapFS{
-				"dir": &fstest.MapFile{Data: []byte("content")}, // Not a directory
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
-		},
-	}
-}
+var noContentOpts = ComparisonOpts{Name: true, Size: true, Mode: true, Content: false}
 
-func deepTestCases() []struct {
+var testCases = []struct {
 	name        string
 	fsA         fstest.MapFS
 	fsB         fstest.MapFS
 	opts        ComparisonOpts
 	shouldError bool
 	expectedLen int
-} {
-	return []struct {
-		name        string
-		fsA         fstest.MapFS
-		fsB         fstest.MapFS
-		opts        ComparisonOpts
-		shouldError bool
-		expectedLen int
-	}{
-		{
-			name: "File contents are equal",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: false,
-			expectedLen: 0,
+}{
+	{
+		name:        "Empty filesystems",
+		fsA:         fstest.MapFS{},
+		fsB:         fstest.MapFS{},
+		opts:        noContentOpts,
+		shouldError: false,
+		expectedLen: 0,
+	},
+	{
+		name: "Identical filesystems",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content")},
 		},
-		{
-			name: "File contents are not equal",
-			fsA: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content A")},
-			},
-			fsB: fstest.MapFS{
-				"file.txt": &fstest.MapFile{Data: []byte("File content B")},
-			},
-			opts:        DefaultComparisonOpts,
-			shouldError: true,
-			expectedLen: 1,
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content")},
 		},
-	}
+		opts:        noContentOpts,
+		shouldError: false,
+		expectedLen: 0,
+	},
+	{
+		name: "Different filesystems",
+		fsA: fstest.MapFS{
+			"fileA.txt": &fstest.MapFile{Data: []byte("File A content")},
+		},
+		fsB: fstest.MapFS{
+			"fileB.txt": &fstest.MapFile{Data: []byte("File B content")},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Filesystem with Info() error",
+		fsA: fstest.MapFS{
+			"file.txt":       &fstest.MapFile{Data: []byte("File content")},
+			"error_info.txt": &fstest.MapFile{Data: []byte("Error content")},
+		},
+		fsB: fstest.MapFS{
+			"file.txt":       &fstest.MapFile{Data: []byte("File content")},
+			"error_info.txt": &fstest.MapFile{Data: []byte("Error content")},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 0,
+	},
+	{
+		name: "Mismatched names",
+		fsA: fstest.MapFS{
+			"file_a.txt": &fstest.MapFile{Data: []byte("hello")},
+		},
+		fsB: fstest.MapFS{
+			"file_b.txt": &fstest.MapFile{Data: []byte("hello")},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched sizes",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("hello")},
+		},
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("hello world")},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched modes",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: 0600},
+		},
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: 0644},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched directory status",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("content")},
+		},
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("content"), Mode: fs.ModeDir},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Directory missing in fsB",
+		fsA: fstest.MapFS{
+			"dir_a": &fstest.MapFile{Mode: fs.ModeDir},
+		},
+		fsB:         fstest.MapFS{}, // Empty MapFS
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched directory names",
+		fsA: fstest.MapFS{
+			"dir_a": &fstest.MapFile{Mode: fs.ModeDir},
+		},
+		fsB: fstest.MapFS{
+			"dir_b": &fstest.MapFile{Mode: fs.ModeDir},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched directory modes",
+		fsA: fstest.MapFS{
+			"dir": &fstest.MapFile{Mode: fs.ModeDir | 0755},
+		},
+		fsB: fstest.MapFS{
+			"dir": &fstest.MapFile{Mode: fs.ModeDir | 0700},
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	{
+		name: "Mismatched directory status",
+		fsA: fstest.MapFS{
+			"dir": &fstest.MapFile{Mode: fs.ModeDir},
+		},
+		fsB: fstest.MapFS{
+			"dir": &fstest.MapFile{Data: []byte("content")}, // Not a directory
+		},
+		opts:        noContentOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
+	// also compare contents
+	{
+		name: "File contents are equal",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content")},
+		},
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content")},
+		},
+		opts:        DefaultComparisonOpts,
+		shouldError: false,
+		expectedLen: 0,
+	},
+	{
+		name: "File contents are not equal",
+		fsA: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content A")},
+		},
+		fsB: fstest.MapFS{
+			"file.txt": &fstest.MapFile{Data: []byte("File content B")},
+		},
+		opts:        DefaultComparisonOpts,
+		shouldError: true,
+		expectedLen: 1,
+	},
 }
 
 func TestEqualFilesystem(t *testing.T) {
-	for _, tc := range testCases() {
+	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			fsA := &errorFS{FS: tc.fsA, triggerInfoError: tc.shouldError}
 			fsB := &errorFS{FS: tc.fsB, triggerInfoError: tc.shouldError}
@@ -226,53 +201,12 @@ func TestEqualFilesystem(t *testing.T) {
 	}
 }
 
-func TestDeepEqualFilesystem(t *testing.T) {
-	tc := testCases()
-	// append deep test cases
-	tc = append(tc, deepTestCases()...)
-
-	for _, tc := range tc {
-		t.Run(tc.name, func(t *testing.T) {
-			fsA := &errorFS{FS: tc.fsA, triggerInfoError: tc.shouldError}
-			fsB := &errorFS{FS: tc.fsB, triggerInfoError: tc.shouldError}
-			err := DeepEqualFilesystem(fsA, fsB, tc.opts)
-			if tc.shouldError {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
-		})
-	}
-}
-
 func TestDiffFS(t *testing.T) {
-	for _, tc := range testCases() {
+	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			fsA := tc.fsA
 			fsB := tc.fsB
 			diffs, err := DiffFS(fsA, fsB, tc.opts)
-
-			if err != nil {
-				t.Fatalf("Unexpected error: %v", err)
-			}
-
-			if len(diffs) != tc.expectedLen {
-				t.Errorf("Expected %d diffs, got %d", tc.expectedLen, len(diffs))
-			}
-		})
-	}
-}
-
-func TestDeepDiffFS(t *testing.T) {
-	tc := testCases()
-	// append deep test cases
-	tc = append(tc, deepTestCases()...)
-
-	for _, tc := range tc {
-		t.Run(tc.name, func(t *testing.T) {
-			fsA := tc.fsA
-			fsB := tc.fsB
-			diffs, err := DeepDiffFS(fsA, fsB, tc.opts)
 
 			if err != nil {
 				t.Fatalf("Unexpected error: %v", err)
