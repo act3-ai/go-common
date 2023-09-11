ae9799c57ed8de3bad54bd3fcecd9b3d46df94c6
Justen Stall <justenstall@gmail.com>
2023-09-11 17:41:23 -0400

    add example manpage and schema embeds in sample

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -2,14 +2,23 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 
 	commands "git.act3-ace.com/ace/go-common/pkg/cmd"
+	"git.act3-ace.com/ace/go-common/pkg/config"
 	"git.act3-ace.com/ace/go-common/pkg/runner"
 	vv "git.act3-ace.com/ace/go-common/pkg/version"
 )
+
+// manpages and schema definitions are embedded here for use in the gendocs and genschema commands
+//
+//go:embed manpages/* schemas/*
+var embeds embed.FS
 
 // getVersionInfo retreives the proper version information for this executable
 func getVersionInfo() vv.Info {
@@ -28,9 +37,29 @@ func main() {
 		Use: "sample",
 	}
 
+	// Create fs.FS rooted in the "manpages" dir
+	manpages, err := fs.Sub(embeds, "manpages")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create fs.FS rooted in the "schemas" dir
+	schemas, err := fs.Sub(embeds, "schemas")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	schemaAssociations := []commands.SchemaAssociation{
+		{
+			Definition: "configuration-schema.json",
+			FileMatch:  config.DefaultConfigValidatePath("ace", "sample", "config.yaml"),
+		},
+	}
+
 	root.AddCommand(
 		commands.NewVersionCmd(info),
-		commands.NewGendocsCmd(nil),
+		commands.NewGendocsCmd(manpages),
+		commands.NewGenschemaCmd(schemas, schemaAssociations),
 	)
 
 	if err := runner.Run(root, "ACE_SAMPLE_VERBOSITY"); err != nil {
