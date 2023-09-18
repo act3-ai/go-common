62abd5d2a2d9881b1b9c9a6a99613ca6af3d5afd
Justen Stall <justen.stall@udri.udayton.edu>
2023-09-18 15:18:23 -0400

    touch up, error message when parsing the verbosity flag should match errors from pflag parsing

--- pkg/runner/run.go
+++ pkg/runner/run.go
@@ -24,43 +24,20 @@ func RunWithContext(ctx context.Context, cmd *cobra.Command, verbosityEnvName st
 	}
 	log := slog.New(slog.NewJSONHandler(os.Stderr, options))
 
-	// flags
-	// var verbosity int
+	// Flags
 	var verbosityFlag []string
 
 	// Set verbosity in the "OnInitialize" function,
 	//  verbosity flag must be parsed before it can be used
 	cobra.OnInitialize(func() {
-		/*
-			slog.Level values from https://pkg.go.dev/log/slog#Level
-			const (
-				LevelDebug Level = -4
-				LevelInfo  Level = 0
-				LevelWarn  Level = 4
-				LevelError Level = 8
-			)
-
-			LevelError is set as the default logging level.
-
-			For slog, "lower" levels mean a chattier logger, so a user is intending to decrease the value of the slog logger's Level when they increase the value of the verbosity flag. Since slog's levels are on multiples of 4, the value of the verbosity flag is multiplied by 4 to easily increase the verbosity to the next level defined. Without the multiplication, a user rerunning a command with a verbosity of 1 to see more logs would see no difference in output, and there is no reason for them to learn the conventions of the Go log/slog package to confidently use our tools.
-		*/
-		// level.Set(slog.LevelError - slog.Level(verbosity*4))
-		l, err := getLoggingLevel(verbosityFlag)
-		if err != nil {
-			panic(err)
-		}
-		level.Set(l)
+		// Convert verbosity flag input to a slog.Level
+		level.Set(getLogLevel(verbosityFlag))
 	})
 
 	cmd.PersistentFlags().StringSliceVarP(&verbosityFlag, "verbosity", "v",
-		[]string{config.EnvOr(verbosityEnvName, "0")},
+		[]string{config.EnvOr(verbosityEnvName, "error")},
 		`Logging verbosity level (also setable with environment variable `+verbosityEnvName+`)
-Aliases:
-  error = 0
-  warn  = 4
-  info  = 8
-  debug = 12
-`)
+Aliases: error=0, warn=4, info=8, debug=12`)
 	x := cmd.PersistentFlags().Lookup("verbosity")
 	x.NoOptDefVal = "warn"
 
@@ -82,12 +59,26 @@ var (
 	}
 )
 
-func getLoggingLevel(flagValue []string) (slog.Level, error) {
+/*
+slog.Level values from https://pkg.go.dev/log/slog#Level
+
+	const (
+		LevelDebug Level = -4
+		LevelInfo  Level = 0
+		LevelWarn  Level = 4
+		LevelError Level = 8
+	)
+
+LevelError is set as the default logging level.
+
+For slog, "lower" levels mean a chattier logger, so a user is intending to decrease the value of the slog logger's Level when they increase the value of the verbosity flag. Since slog's levels are on multiples of 4, the value of the verbosity flag is multiplied by 4 to easily increase the verbosity to the next level defined. Without the multiplication, a user rerunning a command with a verbosity of 1 to see more logs would see no difference in output, and there is no reason for them to learn the conventions of the Go log/slog package to confidently use our tools.
+*/
+func getLogLevel(verbosityFlag []string) slog.Level {
 	// Start with the default level
 	level := slog.LevelError
 
 	// Iterate over flag values, subtracting from the slog level to increase verbosity
-	for _, val := range flagValue {
+	for _, val := range verbosityFlag {
 		if l, ok := verbosityAliases[val]; ok {
 			// Set level to verbosity alias level
 			level -= slog.Level(l)
@@ -95,9 +86,10 @@ func getLoggingLevel(flagValue []string) (slog.Level, error) {
 			// Set integer verbosity
 			level -= slog.Level(l)
 		} else {
-			return level, fmt.Errorf("invalid value %q for flag \"verbosity\"", val)
+			fmt.Printf("Error: invalid argument %q for \"-v, --verbosity\" flag\n", val) //nolint:revive
+			os.Exit(1)
 		}
 	}
 
-	return level, nil
+	return level
 }
