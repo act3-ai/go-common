1caac329d2f118731c4f5d4a0fc8426c8261beb6
Justen Stall <justen.stall@udri.udayton.edu>
2023-09-18 11:12:50 -0400

    fix(runner): restore previous default logging level and verbosity flag behavior

--- pkg/runner/run.go
+++ pkg/runner/run.go
@@ -14,6 +14,7 @@ import (
 
 // RunWithContext will run the root level cobra command but first setup logging with Zap
 func RunWithContext(ctx context.Context, cmd *cobra.Command, verbosityEnvName string) error {
+
 	level := new(slog.LevelVar)
 	options := &slog.HandlerOptions{
 		AddSource: true,
@@ -24,14 +25,30 @@ func RunWithContext(ctx context.Context, cmd *cobra.Command, verbosityEnvName st
 	// flags
 	var verbosity int
 
+	// Set verbosity in the "OnInitialize" function,
+	//  verbosity flag must be parsed before it can be used
 	cobra.OnInitialize(func() {
-		level.Set(slog.Level(verbosity))
+		/*
+			slog.Level values from https://pkg.go.dev/log/slog#Level
+			const (
+				LevelDebug Level = -4
+				LevelInfo  Level = 0
+				LevelWarn  Level = 4
+				LevelError Level = 8
+			)
+
+			LevelError is set as the default logging level.
+
+			For slog, "lower" levels mean a chattier logger, so a user is intending to decrease the value of the slog logger's Level when they increase the value of the verbosity flag. Since slog's levels are on multiples of 4, the value of the verbosity flag is multiplied by 4 to easily increase the verbosity to the next level defined. Without the multiplication, a user rerunning a command with a verbosity of 1 to see more logs would see no difference in output, and there is no reason for them to learn the conventions of the Go log/slog package to confidently use our tools.
+		*/
+		level.Set(slog.LevelError - slog.Level(verbosity*4))
 	})
 
 	cmd.PersistentFlags().IntVarP(&verbosity, "verbosity", "v", config.EnvIntOr(verbosityEnvName, 0),
-		"Logging verbosity level (also setable with environment variable "+verbosityEnvName+")")
+		"Logging verbosity level (also setable with environment variable "+verbosityEnvName+")\n"+
+			"Levels: 0=ERROR, 1=WARN, 2=INFO, 3=DEBUG")
 	x := cmd.PersistentFlags().Lookup("verbosity")
-	x.NoOptDefVal = "0"
+	x.NoOptDefVal = "1"
 
 	ctx = logger.NewContext(ctx, log)
 	return cmd.ExecuteContext(ctx)
