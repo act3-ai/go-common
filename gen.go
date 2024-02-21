f44cd477ed296e7d9bee52ba660fb2a719a3d679
Justen Stall <justen.stall@udri.udayton.edu>
2024-02-21 10:14:54 -0500

    generate demo CLI docs
    Signed-off-by: Justen Stall <justen.stall@udri.udayton.edu>

--- gen.go
+++ gen.go
@@ -1,4 +1,8 @@
 // Package gen is for go:generate directives to generate files.
 package gen
 
+// Generate JSON Schema definitions with genschema package
 //go:generate go run cmd/sample/gen/main.go cmd/sample/schemas
+
+// Generate CLI documentation with gendocs command
+//go:generate go run ./cmd/sample gendocs md cmd/sample/docs/cli --only-commands
