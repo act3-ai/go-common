36c7323c3aa5cfb0853e6d14aacdc3c3b9e60e06
Justen Stall <justen.stall@udri.udayton.edu>
2024-02-23 11:18:53 -0500

    change names in load test
    Signed-off-by: Justen Stall <justen.stall@udri.udayton.edu>

--- pkg/config/load_test.go
+++ pkg/config/load_test.go
@@ -3,5 +3,5 @@ package config
 import "fmt"
 
 func ExampleDefaultConfigSearchPath() {
-	fmt.Println(DefaultConfigSearchPath("ace", "dt", "config.yaml"))
+	fmt.Println(DefaultConfigSearchPath("sample", "cli", "config.yaml"))
 }
