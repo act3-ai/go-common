d7bfbf8113059906f4fd78b5b5bf4c208d5a809a
Justen Stall <2565856-justenstall@users.noreply.gitlab.com>
2024-08-29 14:15:55 -0400

    add SwaggerSpecWriter function to easily write content to a file
    Signed-off-by: Justen Stall <2565856-justenstall@users.noreply.gitlab.com>

--- pkg/oapiutil/oapiutil.go
+++ pkg/oapiutil/oapiutil.go
@@ -2,50 +2,56 @@
 package oapiutil
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/MakeNowJust/heredoc/v2"
 	"gitlab.com/act3-ai/asce/go-common/pkg/httputil"
 )
 
+// SwaggerSpecWriter writes a Swagger UI HTML page for a raw OpenAPI specification.
+func SwaggerSpecWriter(loadSpec func() ([]byte, error), w io.Writer) error {
+	spec, err := loadSpec()
+	if err != nil {
+		return httputil.NewHTTPError(err, 500, "Fetching OpenAPI spec")
+	}
+
+	_, err = w.Write([]byte(heredoc.Docf(`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="description" content="API documentation" />
+			<title>API documentation</title>
+			<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
+		</head>
+		<body>
+			<div id="swagger-ui"></div>
+			<script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
+			<script crossorigin>
+			window.onload = () => {
+				window.ui = SwaggerUIBundle({
+					spec: %s,
+					dom_id: '#swagger-ui',
+					docExpansion: false,
+					defaultModelRendering: 'model',
+				});
+			};
+			</script>
+		</body>
+		</html>`, string(spec))))
+	if err != nil {
+		return fmt.Errorf("writing HTML: %w", err)
+	}
+	return nil
+}
+
 // SwaggerSpecHandler creates an [http.Handler] to serve the Swagger UI for a raw OpenAPI specification.
 func SwaggerSpecHandler(loadSpec func() ([]byte, error)) http.Handler {
 	return httputil.RootHandler(func(w http.ResponseWriter, r *http.Request) error {
-		spec, err := loadSpec()
-		if err != nil {
-			return httputil.NewHTTPError(err, 500, "Fetching OpenAPI spec")
-		}
-
-		_, err = w.Write([]byte(heredoc.Docf(`
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="description" content="SwaggerUI" />
-				<title>SwaggerUI</title>
-				<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
-			</head>
-			<body>
-				<div id="swagger-ui"></div>
-				<script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
-				<script crossorigin>
-				window.onload = () => {
-					window.ui = SwaggerUIBundle({
-						spec: %s,
-						dom_id: '#swagger-ui',
-						docExpansion: false,
-						defaultModelRendering: 'model',
-					});
-				};
-				</script>
-			</body>
-			</html>`, string(spec))))
-		if err != nil {
-			return httputil.NewHTTPError(err, 500, "Writing HTML")
-		}
-
-		return nil
+		return SwaggerSpecWriter(loadSpec, w)
 	})
 }
 
