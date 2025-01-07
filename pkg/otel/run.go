41fdd7e570ba9b3bab93e6f92d0c1c3c1b97152d
Nathan Joslin <nathan.joslin@udri.udayton.edu>
2025-01-07 13:54:21 -0500

    comments an organization

--- pkg/otel/run.go
+++ pkg/otel/run.go
@@ -14,8 +14,6 @@ import (
 	"gitlab.com/act3-ai/asce/go-common/pkg/runner"
 )
 
-const otelErrKey = "otelErr"
-
 // RunWithContext will run the root level cobra command, with the provided
 // OpenTelemetry configuration. It calls cfg.Init and cfg.Close appropriately.
 func RunWithContext(ctx context.Context, cmd *cobra.Command, cfg *Config, verbosityEnvName string) error {
@@ -29,6 +27,7 @@ func RunWithContext(ctx context.Context, cmd *cobra.Command, cfg *Config, verbos
 	}
 	defer cfg.Shutdown()
 
+	// create a single logger with a handler for stderr and otel
 	slogRouter := slogmulti.Router()
 	slogRouter.Add(runner.SetupLoggingHandler(cmd, verbosityEnvName), allowAll())
 	if cfg.logProvider != nil {
@@ -42,21 +41,23 @@ func RunWithContext(ctx context.Context, cmd *cobra.Command, cfg *Config, verbos
 
 	otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
 		// attribute tells slogRounter to not send these logs to telemetry, i.e. logged only locally
-		// TODO: Ideally, we would be able to filter out only the failures to emit log telemetry.
-		// This filtering is only necessary to prevent the log telemetry emit recursion (see ignoreOtel).
 		logger.FromContext(ctx).With(otelErrKey, "otel_emit_err").ErrorContext(ctx, "failed to emit telemetry", "error", err)
 	}))
 
 	return cmd.ExecuteContext(ctx)
 }
 
+const otelErrKey = "otelErr"
+
 // ignoreOtel filters out error logs provided by the otel ErrorHandler
-// indicating a log telemetry emit error, ensuring they're only emitted locally. It
-// prevents infinite recursion in case log telemetry fails, e.g.
+// indicating a log telemetry emit error, ensuring they're only emitted locally.
+// It prevents infinite recursion in case log telemetry fails, e.g.
 // emit log telemetry --> failure to send logs to endpoint -->
 // log telemetry emit err --> emit log telemetry --> ...
 func ignoreOtel() func(ctx context.Context, r slog.Record) bool {
 	return func(ctx context.Context, r slog.Record) bool {
+		// TODO: Ideally, we filter out only failures to emit log telemetry. but
+		// this is not trivial.
 		ok := true
 		r.Attrs(func(a slog.Attr) bool {
 			if a.Key == otelErrKey {
@@ -69,6 +70,7 @@ func ignoreOtel() func(ctx context.Context, r slog.Record) bool {
 	}
 }
 
+// allowAll performs no log filtering.
 func allowAll() func(ctx context.Context, r slog.Record) bool {
 	return func(ctx context.Context, r slog.Record) bool {
 		return true
