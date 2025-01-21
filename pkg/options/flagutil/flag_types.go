8ca2397f66c4a0f4f787a148245fddfb0263694a
Justen Stall <2565856-justenstall@users.noreply.gitlab.com>
2025-01-21 09:57:53 -0500

    add options framework for unified definition of flags and environment variable overrides
    Signed-off-by: Justen Stall <2565856-justenstall@users.noreply.gitlab.com>

---
+++ pkg/options/flagutil/flag_types.go
@@ -0,0 +1,136 @@
+package flagutil
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+/* Implementations are almost identical to the StringTo* implementation in the pflag project. */
+
+// -- stringToBool Value
+type stringToBoolValue struct {
+	value   *map[string]bool
+	changed bool
+}
+
+func newStringToBoolValue(val map[string]bool, p *map[string]bool) *stringToBoolValue {
+	ssv := new(stringToBoolValue)
+	ssv.value = p
+	*ssv.value = val
+	return ssv
+}
+
+// Format: a=true,b=false,c
+func (s *stringToBoolValue) Set(val string) error {
+	ss := strings.Split(val, ",")
+	out := make(map[string]bool, len(ss))
+	for _, pair := range ss {
+		// TODO: interpret omitted "=" as "true"
+		kv := strings.SplitN(pair, "=", 2)
+		if len(kv) != 2 {
+			return fmt.Errorf("%s must be formatted as key=value", pair)
+		}
+		var err error
+		out[kv[0]], err = strconv.ParseBool(kv[1])
+		if err != nil {
+			return fmt.Errorf("parsing value for %q: %w", kv[0], err)
+		}
+	}
+	if !s.changed {
+		*s.value = out
+	} else {
+		for k, v := range out {
+			(*s.value)[k] = v
+		}
+	}
+	s.changed = true
+	return nil
+}
+
+func (s *stringToBoolValue) Type() string {
+	return "stringToBool"
+}
+
+func (s *stringToBoolValue) String() string {
+	var buf bytes.Buffer
+	i := 0
+	for k, v := range *s.value {
+		if i > 0 {
+			buf.WriteRune(',')
+		}
+		buf.WriteString(k)
+		buf.WriteRune('=')
+		buf.WriteString(strconv.FormatBool(v))
+		i++
+	}
+	return "[" + buf.String() + "]"
+}
+
+// -- stringToOptString Value
+type stringToOptStringValue struct {
+	value   *map[string]*string
+	changed bool
+}
+
+func newStringToOptStringValue(val map[string]*string, p *map[string]*string) *stringToOptStringValue {
+	ssv := new(stringToOptStringValue)
+	ssv.value = p
+	*ssv.value = val
+	return ssv
+}
+
+// Format: a,b=2
+func (s *stringToOptStringValue) Set(val string) error {
+	var ss []string
+	r := csv.NewReader(strings.NewReader(val))
+	var err error
+	ss, err = r.Read()
+	if err != nil {
+		return err
+	}
+
+	out := make(map[string]*string, len(ss))
+	for _, pair := range ss {
+		k, v, found := strings.Cut(pair, "=")
+		if !found {
+			out[k] = nil
+		} else {
+			out[k] = &v
+		}
+	}
+	if !s.changed {
+		*s.value = out
+	} else {
+		for k, v := range out {
+			(*s.value)[k] = v
+		}
+	}
+	s.changed = true
+	return nil
+}
+
+func (s *stringToOptStringValue) Type() string {
+	return "stringToOptString"
+}
+
+func (s *stringToOptStringValue) String() string {
+	records := make([]string, 0, len(*s.value)>>1)
+	for k, v := range *s.value {
+		if v != nil {
+			records = append(records, k+"="+*v)
+		} else {
+			records = append(records, k)
+		}
+	}
+
+	var buf bytes.Buffer
+	w := csv.NewWriter(&buf)
+	if err := w.Write(records); err != nil {
+		panic(err)
+	}
+	w.Flush()
+	return "[" + strings.TrimSpace(buf.String()) + "]"
+}
---
+++ pkg/options/flagutil/values.go
@@ -0,0 +1,109 @@
+package flagutil
+
+import (
+	"log/slog"
+	"maps"
+	"time"
+
+	"github.com/spf13/pflag"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ValueOr returns either the flag value or the default.
+func ValueOr[T any](f *pflag.Flag, flagValue T, def T) T {
+	if !f.Changed {
+		return def
+	}
+	slog.Info("overriding config with flag value",
+		slog.String("flag", f.Name),
+		slog.Any("value", flagValue),
+	)
+	return flagValue
+}
+
+// DurationOr parses a string flag into a [metav1.Duration], returning def if the flag was not set.
+func DurationOr(f *pflag.Flag, flagValue string, def *metav1.Duration) *metav1.Duration {
+	if !f.Changed {
+		return def
+	}
+	pd, err := time.ParseDuration(flagValue)
+	if err != nil {
+		slog.Error("parsing duration flag value",
+			slog.Any("err", err),
+			slog.String("flag", f.Name),
+			slog.String("value", flagValue),
+		)
+		return def
+	}
+	slog.Info("overriding config with flag value",
+		slog.String("flag", f.Name),
+		slog.String("value", flagValue),
+	)
+	return &metav1.Duration{Duration: pd}
+}
+
+// SetMapKeysOr uses values given to a string slice flag to set the keys of a configuration map.
+//
+// For keys already set in the configuration map, the current value will be preserved.
+//
+// For keys not found in the configuration map, they will be set to the empty value of T.
+func SetMapKeysOr[T any](f *pflag.Flag, flagValue []string, in map[string]T) map[string]T {
+	if !f.Changed {
+		return in
+	}
+	slog.Info("overriding config with flag value",
+		slog.String("flag", f.Name),
+		slog.Any("value", flagValue),
+	)
+	return setMapKeys(flagValue, in)
+}
+
+func setMapKeys[M ~map[K]V, K comparable, V any](keys []K, in M) M {
+	out := make(M, len(keys))
+	for _, t := range keys {
+		if in == nil {
+			var empty V
+			out[t] = empty
+		} else {
+			out[t] = in[t]
+		}
+	}
+	return out
+}
+
+// SetFunc describes a function to set map values.
+type SetFunc[V1, V2 any] func(flagValue V1, entry *V2, ok bool) (skip bool)
+
+// SetMapValuesOr uses values given to a map-style flag to update a configuration map as defined by the SetFunc.
+func SetMapValuesOr[M1 ~map[K]V1, M2 ~map[K]V2, K comparable, V1, V2 any](
+	f *pflag.Flag, flagValues M1, in M2, setter SetFunc[V1, V2]) M2 {
+	if !f.Changed {
+		return in
+	}
+	slog.Info("overriding config with flag value",
+		slog.String("flag", f.Name),
+		slog.Any("value", flagValues),
+	)
+	return setMapValues(flagValues, in, setter)
+}
+
+// merges the flag values with the given map. check unit tests for implementation details.
+func setMapValues[M1 ~map[K]V1, M2 ~map[K]V2, K comparable, V1, V2 any](
+	flagValues M1, in M2, setter SetFunc[V1, V2]) M2 {
+	out := maps.Clone(in)
+	for k, flagValue := range flagValues {
+		entry, ok := in[k]
+		skip := setter(flagValue, &entry, ok)
+		switch {
+		case skip:
+			// Skip updating entry if indicated
+			continue
+		case out == nil:
+			out = M2{k: entry}
+		default:
+			out[k] = entry
+		}
+	}
+	return out
+}
