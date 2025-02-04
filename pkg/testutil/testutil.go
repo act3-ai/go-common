f48596c0e0a9bb192a9074d1ff72c0ad7f6fd2cf
Justen Stall <2565856-justenstall@users.noreply.gitlab.com>
2025-02-04 13:11:39 -0500

    add AssertPanicsIf function to testutil
    Signed-off-by: Justen Stall <2565856-justenstall@users.noreply.gitlab.com>

--- pkg/testutil/testutil.go
+++ pkg/testutil/testutil.go
@@ -16,6 +16,15 @@ func AssertErrorIf(t *testing.T, wantErr bool, err error, msgAndArgs ...any) boo
 	return assert.NoError(t, err, msgAndArgs...)
 }
 
+// AssertPanicsIf asserts that function panics if wantPanic is true, or does not panic if wantPanic is false.
+func AssertPanicsIf(t *testing.T, wantPanic bool, f assert.PanicTestFunc, msgAndArgs ...any) bool {
+	t.Helper()
+	if wantPanic {
+		return assert.Panics(t, f, msgAndArgs...)
+	}
+	return assert.NotPanics(t, f, msgAndArgs...)
+}
+
 // AssertNilIf asserts that expected and actual are either both nil or both non-nil.
 func AssertNilIf(t *testing.T, wantNil bool, actual any, msgAndArgs ...any) bool {
 	t.Helper()
--- pkg/testutil/testutil_test.go
+++ pkg/testutil/testutil_test.go
@@ -30,6 +30,31 @@ func TestAssertErrorIf(t *testing.T) {
 	}
 }
 
+func TestAssertPanicsIf(t *testing.T) {
+	var panicFunc assert.PanicTestFunc = func() { panic("test") }
+	var noPanicFunc assert.PanicTestFunc = func() { return }
+	type args struct {
+		wantPanic bool
+		f         assert.PanicTestFunc
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"true/pass", args{true, panicFunc}, true},
+		{"true/fail", args{true, noPanicFunc}, false},
+		{"false/pass", args{false, noPanicFunc}, true},
+		{"false/fail", args{false, panicFunc}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AssertPanicsIf(&testing.T{}, tt.args.wantPanic, tt.args.f)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
 func TestAssertNilIf(t *testing.T) {
 	type args struct {
 		wantNil bool
