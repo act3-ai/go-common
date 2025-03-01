47e5467c2e8366a3a713811075091411113b78fa
Kyle M. Tarplee <kyle.tarplee@udri.udayton.edu>
2025-03-01 10:35:47 -0500

    feat: added OTEL_INSTRUMENTATION_ENABLED env to be explicit about turning on OTEL support

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -4,26 +4,17 @@ package main
 import (
 	"context"
 	"embed"
-	"fmt"
 	"os"
-	"strings"
 
-	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 
 	commands "gitlab.com/act3-ai/asce/go-common/pkg/cmd"
 	"gitlab.com/act3-ai/asce/go-common/pkg/config"
 	"gitlab.com/act3-ai/asce/go-common/pkg/embedutil"
-	"gitlab.com/act3-ai/asce/go-common/pkg/options"
-	"gitlab.com/act3-ai/asce/go-common/pkg/options/cobrautil"
-	"gitlab.com/act3-ai/asce/go-common/pkg/options/flagutil"
-	"gitlab.com/act3-ai/asce/go-common/pkg/options/optionshelp"
 	"gitlab.com/act3-ai/asce/go-common/pkg/otel"
-	"gitlab.com/act3-ai/asce/go-common/pkg/termdoc"
-	"gitlab.com/act3-ai/asce/go-common/pkg/termdoc/codefmt"
+	"gitlab.com/act3-ai/asce/go-common/pkg/runner"
 	vv "gitlab.com/act3-ai/asce/go-common/pkg/version"
 )
 
@@ -47,30 +38,13 @@ func getVersionInfo() vv.Info {
 	return info
 }
 
-func mainSetup() (context.Context, *cobra.Command, *otel.Config, error) {
-	info := getVersionInfo()        // Load the version info from the build
-	root := newSample(info.Version) // Create the root command
+const verbosityEnvName = "ACE_SAMPLE_VERBOSITY"
 
+func mainE(args []string) error {
 	ctx := context.Background()
 
-	r, err := resource.New(
-		ctx,
-		resource.WithAttributes(
-			semconv.ServiceName("sample"),
-			semconv.ServiceVersion(info.Version),
-		),
-		resource.WithFromEnv(),
-		resource.WithTelemetrySDK(),
-		resource.WithOS(),
-	)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("OTEL resource setup failed: %w", err)
-	}
-
-	otelCfg := &otel.Config{
-		Resource: r,
-		// Hardcoded exporters may be added here...
-	}
+	info := getVersionInfo()        // Load the version info from the build
+	root := newSample(info.Version) // Create the root command
 
 	schemaAssociations := []commands.SchemaAssociation{
 		{
@@ -100,207 +74,33 @@ func mainSetup() (context.Context, *cobra.Command, *otel.Config, error) {
 		commands.NewGendocsCmd(docs),
 		commands.NewGenschemaCmd(schemas, schemaAssociations),
 	)
-	return ctx, root, otelCfg, nil
-}
 
-func mainE(args []string) error {
-	ctx, root, otelCfg, err := mainSetup()
-	if err != nil {
-		return err
-	}
 	root.SetArgs(args)
 
-	// Run root command with OTel instrumentation enabled.
-	return otel.Run(ctx, root, otelCfg, "ACE_SAMPLE_VERBOSITY")
+	if v := os.Getenv("OTEL_INSTRUMENTATION_ENABLED"); v == "true" {
+		r, _ := resource.New(
+			ctx,
+			resource.WithAttributes(
+				semconv.ServiceName("sample"),
+				semconv.ServiceVersion(info.Version),
+			),
+			resource.WithFromEnv(),
+			resource.WithTelemetrySDK(),
+			resource.WithOS(),
+		)
+
+		otelCfg := &otel.Config{
+			Resource: r,
+		}
+
+		// Run root command with OTel instrumentation enabled.
+		return otel.Run(ctx, root, otelCfg, verbosityEnvName)
+	}
+	return runner.Run(ctx, root, verbosityEnvName)
 }
 
 func main() {
 	if err := mainE(os.Args[1:]); err != nil {
 		os.Exit(1)
 	}
-}
-
-//go:embed docs/testfile.md
-var testFile string
-
-// Define command options.
-type sampleAction struct {
-	greeting string
-	name     string
-	count    int
-	excited  bool
-}
-
-// NOTE Often the main command is created in another package and imported
-func newSample(version string) *cobra.Command {
-	action := &sampleAction{}
-
-	root := &cobra.Command{
-		Use: "sample",
-		Example: heredoc.Doc(`
-			# Run sample:
-			sample
-			
-			# Run sample with name set by flag:
-			sample --name "Foo"
-			
-			# Run sample with name set by environment variable:
-			ACE_SAMPLE_NAME="Foo" sample`),
-		Version: version,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			// Parse environment variables to set the value of flags,
-			// if they have an environment variable defined.
-			// Environment variables can be set with flagutil.SetEnvName.
-			// The flag creation functions in pkg/options/flags.go set an
-			// environment variable for the flag if Option.Env is set.
-			return cobrautil.ParseEnvOverrides(cmd)
-		},
-		Run: func(cmd *cobra.Command, args []string) {
-			if action.name == "" {
-				action.name = "world"
-			}
-			suffix := ""
-			if action.excited {
-				suffix = "!"
-			}
-			for range action.count {
-				cmd.Println(action.greeting + " " + action.name + suffix)
-			}
-		},
-	}
-
-	// Add flags to the command
-	optionGroups := addFlags(root.Flags(), action)
-
-	// Formatting options to style the help command text.
-	//
-	// These are just an example of some styling and content choices that can be made.
-	formatOptions := cobrautil.UsageFormatOptions{
-		Format: cobrautil.Formatter{
-			// Format headers as uppercase.
-			Header: strings.ToUpper,
-			// Format command examples as Bash code snippets
-			// with faint comments.
-			Example: func(s string) string {
-				return termdoc.AutoCodeFormat().Format(s, codefmt.Bash)
-			},
-		},
-		// Options for the display of flag usages.
-		FlagOptions: flagutil.UsageFormatOptions{
-			// Override flag type name with type name configured by `options` package.
-			FormatType: func(flag *pflag.Flag, typeName string) string {
-				opt := options.FromFlag(flag)
-				if opt.FlagType != "" {
-					typeName = opt.FlagType
-				}
-				return typeName
-			},
-			// If there is a configured environment variable for the flag,
-			// add environment variable name to the usage string
-			FormatUsage: func(flag *pflag.Flag, usage string) string {
-				opt := options.FromFlag(flag)
-				if opt.Env != "" {
-					usage += fmt.Sprintf(" (env: %s)", opt.Env)
-				}
-				return usage
-			},
-		},
-		// Set local flags to be separated by grouping.
-		LocalFlags: cobrautil.FlagGroupingOptions{
-			GroupFlags: true,
-		},
-	}
-
-	// Set custom usage function to format command
-	// help text using our special formatting.
-	cobrautil.WithCustomUsage(root, formatOptions)
-
-	// Set custom formatting for the gendocs command
-	// so generated docs match the format of the help text.
-	embedutil.SetUsageFormat(formatOptions)
-
-	termMD := termdoc.AutoMarkdownFormat()
-
-	root.AddCommand(
-		optionshelp.Command(
-			"sample-config", "Help for sample CLI configuration", optionGroups, termMD),
-		// Add "Additional Help Topic" command that simply prints documentation.
-		termdoc.AdditionalHelpTopic(
-			"testfile", "Help command that displays the test file", testFile, termMD),
-	)
-
-	return root
-}
-
-func addFlags(f *pflag.FlagSet, action *sampleAction) []*options.Group {
-	// Define each option
-	name := &options.Option{
-		Type:          options.String,
-		Default:       "",
-		JSON:          "name",
-		Env:           "ACE_SAMPLE_NAME", // flagutil.ParseEnvOverrides uses this to set the value.
-		Flag:          "name",
-		FlagShorthand: "n",
-		Short:         "Your name.",
-		Long: heredoc.Doc(`
-			Name of the sample CLI's user.`),
-	}
-	greeting := &options.Option{
-		Type:          options.String,
-		Default:       "Hello",
-		JSON:          "",
-		Env:           "ACE_SAMPLE_GREETING",
-		Flag:          "greeting",
-		FlagShorthand: "g",
-		Short:         "Greeting for the user.",
-		Long:          ``,
-	}
-	count := &options.Option{
-		Type:          options.Integer,
-		Default:       "1",
-		JSON:          "",
-		Env:           "ACE_SAMPLE_COUNT",
-		Flag:          "count",
-		FlagShorthand: "c",
-		Short:         "Number of greetings to output.",
-		Long:          ``,
-	}
-	excited := &options.Option{
-		Type:          options.Boolean,
-		Default:       "false",
-		JSON:          "",
-		Env:           "ACE_SAMPLE_EXCITED",
-		Flag:          "excited",
-		FlagShorthand: "e",
-		Short:         "Greet with excitement.",
-		Long:          ``,
-	}
-
-	// Create a group for the options
-	group := &options.Group{
-		Name:        "example",
-		Description: "Example options",
-		Options: []*options.Option{
-			name,
-			greeting,
-			count,
-			excited,
-		},
-	}
-
-	// Add flags to the command.
-	nameFlag := options.StringVar(f, &action.name, "", name)
-	greetingFlag := options.StringVar(f, &action.greeting, "Hello", greeting)
-	countFlag := options.IntVar(f, &action.count, 1, count)
-	excitedFlag := options.BoolVar(f, &action.excited, false, excited)
-
-	// Mark the flags as grouped to organize in help text.
-	options.GroupFlags(group,
-		nameFlag,
-		greetingFlag,
-		countFlag,
-		excitedFlag,
-	)
-
-	return []*options.Group{group}
 }
--- cmd/sample/main_test.go
+++ cmd/sample/main_test.go
@@ -2,34 +2,7 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
-
-func TestMainSetup(t *testing.T) {
-	ctx, rootCmd, otelCfg, err := mainSetup()
-	assert.NoError(t, err)
-
-	// Check if context is not nil
-	assert.NotNil(t, ctx, "Context should not be nil")
-
-	// Check if root command is not nil
-	assert.NotNil(t, rootCmd, "Root command should not be nil")
-
-	// Check if otelCfg is not nil
-	assert.NotNil(t, otelCfg, "Otel config should not be nil")
-
-	// Check if the root command has the expected subcommands
-	commandNames := map[string]struct{}{}
-	for _, cmd := range rootCmd.Commands() {
-		commandNames[cmd.Name()] = struct{}{}
-	}
-	assert.Contains(t, commandNames, "version")
-
-	rootCmd.SetContext(ctx)
-	// test root command PersistentPreRun by running the main func
-	// rootCmd.PersistentPreRun(rootCmd, []string{})
-}
 
 func Test_mainE(t *testing.T) {
 	tests := []struct {
