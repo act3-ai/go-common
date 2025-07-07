711f2e8058df4f8aab96227890318d721497b33f
Justen Stall <39888103+justenstall@users.noreply.github.com>
2025-07-07 15:43:40 -0400

    io.Reader accepting spec handler
    Signed-off-by: Justen Stall <39888103+justenstall@users.noreply.github.com>

--- pkg/oapiutil/oapiutil.go
+++ pkg/oapiutil/oapiutil.go
@@ -2,6 +2,8 @@
 package oapiutil
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 
 	"github.com/act3-ai/go-common/pkg/httputil"
@@ -9,18 +11,25 @@ import (
 
 // SpecHandler creates an [http.Handler] to serve an OpenAPI specification.
 func SpecHandler(loadSpec func() ([]byte, error)) http.Handler {
-	return httputil.RootHandler(func(w http.ResponseWriter, _ *http.Request) error {
+	return httputil.RootHandler(func(w http.ResponseWriter, r *http.Request) error {
 		spec, err := loadSpec()
 		if err != nil {
 			return httputil.NewHTTPError(err, http.StatusNotFound, "Fetching OpenAPI spec")
 		}
+		// Call SpecReaderHandler
+		SpecReaderHandler(bytes.NewReader(spec)).ServeHTTP(w, r)
+		return nil
+	})
+}
 
+// SpecHandler creates an [http.Handler] to serve an OpenAPI specification.
+func SpecReaderHandler(r io.Reader) http.Handler {
+	return httputil.RootHandler(func(w http.ResponseWriter, _ *http.Request) error {
 		w.Header().Set("Content-Type", "application/json")
-		_, err = w.Write(spec)
+		_, err := io.Copy(w, r)
 		if err != nil {
 			return httputil.NewHTTPError(err, http.StatusInternalServerError, "Writing OpenAPI spec")
 		}
-
 		return nil
 	})
 }
